b97f829178b9e4a31defc20b70f3b8faf4164f59
Michael Kernaghan <michael@debian11.mike>
2022-10-23 12:58:35 -0700

    working model

---
+++ sheepGameCases.ts
@@ -0,0 +1,277 @@
+let position: number = 0
+let farmEvent
+switch (position) {
+    case 0: {
+        woolSale()
+        break;
+    }
+    case 1: {
+        stockSale();
+        break;
+    }
+    case 2: {
+        sheepDipping();
+        break;
+    }
+    case 3: {
+        studRam(0);
+        break;
+    }
+    case 4: {
+        tuckerBag();
+        break;
+    }
+    case 5: {
+        boreDriesUp();
+        break;
+    }
+    case 6: {
+        visitingTown();
+        break;
+    }
+    case 7: {
+        visitingTown();
+        break;
+    }
+    case 8: {
+        drenchWorms();
+        break;
+    }
+    case 9: {
+        tuckerBag();
+        break;
+    }
+    case 10: {
+        floodDamage();
+        break;
+    }
+    case 11: {
+        tuckerBag();
+        break;
+    }
+    case 12: {
+        stockSale();
+        break;
+    }
+    case 13: {
+        jetSheep();
+        break;
+    }
+    case 14: {
+        poisonRabbits();
+        break;
+    }
+    case 15: {
+        stockSale();
+        break;
+    }
+    case 16: {
+        footrotTreatment();
+        break;
+    }
+    case 17: {
+        stockSale();
+        break;
+    }
+    case 18: {
+        studRam(1);
+        break;
+    }
+    case 19: {
+        tuckerBag();
+        break;
+    }
+    case 20: {
+        fencingRepairs();
+        break;
+    }
+    case 21: {
+        sprayWeeds();
+        break;
+    }
+    case 22: {
+        localDrought();
+        break;
+    }
+    case 23: {
+        drenchFluke();
+        break;
+    }
+    case 24: {
+        tuckerBag();
+        break;
+    }
+    case 25: {
+        stockSale();
+        break;
+    }
+    case 26: {
+        studRam(2);
+        break;
+    }
+    case 27: {
+        localRain();
+        break;
+    }
+    case 28: {
+        stockSale();
+        break;
+    }
+    case 29: {
+        vaccinateKidney();
+        break;
+    }
+    case 30: {
+        studRam(3);
+        break;
+    }
+    case 31: {
+        tuckerBag();
+        break;
+    }
+    case 32: {
+        stockSale();
+        break;
+    }
+    case 33: {
+        studRamDies();
+        break;
+    }
+    case 34: {
+        drillingExpenses();
+        break;
+    }
+    case 35: {
+        tuckerBag();
+        break;
+    }
+    case 36: {
+        stockSale();
+        break;
+    }
+
+    case 37: {
+        fertilizePasture();
+        break;
+    }
+    case 38: {
+        stockSale();
+        break;
+    }
+    case 39: {
+        studRam(4);
+        break;
+    }
+    case 40: {
+        shearingCosts();
+        break;
+    }
+    case 41: {
+        tuckerBag();
+        break;
+    }
+    case 42: {
+        stockSale();
+        break;
+    }
+    case 43: {
+        localDrought();
+        break;
+    }
+    default: {
+        woolSale();
+        break;
+    }
+}
+
+
+
+function woolSale() {
+    throw new Error("Function not implemented.");
+}
+
+
+function stockSale() {
+    throw new Error("Function not implemented.");
+}
+
+function sheepDipping() {
+    throw new Error("Function not implemented.");
+}
+
+function studRam(arg0: number) {
+    throw new Error("Function not implemented.");
+}
+
+function tuckerBag() {
+    throw new Error("Function not implemented.");
+}
+
+function boreDriesUp() {
+    throw new Error("Function not implemented.");
+}
+
+function visitingTown() {
+    throw new Error("Function not implemented.");
+}
+
+function drenchWorms() {
+    throw new Error("Function not implemented.");
+}
+
+function floodDamage() {
+    throw new Error("Function not implemented.");
+}
+
+function jetSheep() {
+    throw new Error("Function not implemented.");
+}
+
+function poisonRabbits() {
+    throw new Error("Function not implemented.");
+}
+
+function footrotTreatment() {
+    throw new Error("Function not implemented.");
+}
+
+function fencingRepairs() {
+    throw new Error("Function not implemented.");
+}
+
+function sprayWeeds() {
+    throw new Error("Function not implemented.");
+}
+
+function localDrought() {
+    throw new Error("Function not implemented.");
+}
+
+function drenchFluke() {
+    throw new Error("Function not implemented.");
+}
+
+function localRain() {
+    throw new Error("Function not implemented.");
+}
+
+function vaccinateKidney() {
+    throw new Error("Function not implemented.");
+}
+
+function studRamDies() {
+    throw new Error("Function not implemented.");
+}
+
+function drillingExpenses() {
+    throw new Error("Function not implemented.");
+}
+
+function fertilizePasture() {
+    throw new Error("Function not implemented.");
+}
+
+function shearingCosts() {
+    throw new Error("Function not implemented.");
+}
+
