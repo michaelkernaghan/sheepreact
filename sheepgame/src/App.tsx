aa69ae34b201ef31a572f5dc3146cfaa7f175ef1
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2022-10-23 23:42:00 -0700

    added all the Tucker Bag cards

--- sheepgame/src/App.tsx
+++ sheepgame/src/App.tsx
@@ -25,7 +25,7 @@ function stockSellPrice() {
 }
 
 function randomForTuckerBag() {
-  var randomDice1 = Math.floor(5 * Math.random())
+  var randomDice1 = Math.floor(24 * Math.random())
   return randomDice1;
 }
 
@@ -51,56 +51,167 @@ const Move: React.FC = () => {
   let new_position = (positionValue + dieRoll)
   const modded_position = new_position % 44
   // WOOLSALE
-  if (new_position > 43) {
+  if (modded_position === 0 || new_position === 45 || new_position === 46 || new_position === 47 || new_position === 48
+    || new_position === 49 || new_position === 50 || new_position === 51 || new_position === 52 || new_position === 53
+    || new_position === 54 || new_position === 55) {
+    message = 'It\'s the start of another Year!';
     woolsale = 'Yes, good luck!';
     woolSaleYield = pens * 250
-    //setMoney(moneyValue + woolSaleYield)
+    //setMoney(() => (moneyValue + woolSaleYield))
   }
   // HAYMAKING
   if (modded_position > 36 && modded_position < 43) {
-      haymaking = 'Yes';
-    }
+    haymaking = 'Yes';
+  }
   // TUCKER BAG
   if (modded_position === 4 || modded_position === 9 || modded_position === 11 || modded_position === 19 || modded_position === 24
     || modded_position === 31 || modded_position === 35 || modded_position === 41) {
     let whichCard = randomForTuckerBag()
     switch (whichCard) {
-      //Grass Fire
       case 0: {
         let howMany = ((sheepAmount / 200) / 2)
         let price = stockSellPrice()
         stockSaleYield = -1 * howMany * price
-        if (moneyValue > stockSaleYield) {
-          //   setMoney(moneyValue + stockSaleYield)
-          //   setSheep(sheepAmount / 2)
-        }
+        // setMoney(moneyValue + stockSaleYield)
+        // setSheep(sheepAmount / 2)
         message = 'Grass Fires destroy Haystack and most of the Pasture Fodder. Half your sheep must be sold at market price'
         stockSaleMessage = "You sold " + howMany * 200 + " sheep for " + stockSaleYield + "."
         break;
       }
-      // Income Tax
       case 1: {
-        message = 'You must pay $300 Income Tax.'
-        setMoney(moneyValue - 300)
+        message = 'You must pay $50 Income Tax for each Natural Pasture Paddock and $200 for each $1,000 Cash held, to nearest thousand.'
+        // let taxOnCash = Math.floor(moneyValue / 1000) * 200
+        //setMoney(moneyValue - (300 + taxOnCash))
         break;
       }
-      // GENERAL DROUGHT
       case 3: {
         message = 'There is now a General Drought.'
-        droughtMessage = 'General Drought'
+        droughtMessage = 'General drought'
         //setDrought(true)
         break;
       }
-      // CHAMPION RAM
       case 4: {
         message = 'You have won a Trophy for Champion Ram and recieve $400.'
         setMoney(moneyValue + 400)
         break;
       }
-      // FIRE
       case 5: {
         message = 'Fire destroys Haystack and damges Out-Buildings at a cost of $500.'
         setMoney(moneyValue - 500)
+        break;
+      }
+      case 6: {
+        message = 'You buy Fire Fighting Equipment for $150.'
+        setMoney(moneyValue - 150)
+        break;
+      }
+
+      case 7: {
+        message = 'You get an Insurance Policy for a Stud Ram. If the Stud Ram dies you get $500.'
+        break;
+      }
+      case 8: {
+        message = 'Your selling price at next stock sale is increased by 20%.'
+        break;
+      }
+      case 9: {
+        message = 'Purchase a new shearing shed. $1,000 each if less than 15 pens, otherwise $2,000 each'
+        if (pens < 15) {
+          setMoney(moneyValue - (pens * 1000))
+        }
+        if (pens >= 15) {
+          setMoney(moneyValue - (pens * 2000))
+        }
+        break;
+      }
+      case 10: {
+        message = 'By careful management you have eradicated Foot Rot. You are exempt from future treatment costs.'
+        break;
+      }
+      case 11: {
+        message = 'As a result of severe Blowfly Wave, and your failure to take precautionary action your next Wool Cheque reduces by 10%. No loss if you land on "Jet Sheep" before your next Wool Cheque.'
+        break;
+      }
+      case 12: {
+        message = 'If your property is not fully stocked, receive Agistment fees of $600.'
+        if (sheepAmount < 3000) {
+          setMoney(moneyValue + 600)
+        }
+        break;
+      }
+      case 13: {
+        message = 'General rain breaks drought'
+        droughtMessage = 'No drought'
+        break;
+      }
+      case 14: {
+        message = 'You are injured by Tractor as a result of carelessness.'
+        break;
+      }
+      case 15: {
+        message = 'Severe infestation of Lucerne Flea and red Legged Earth Mite reduces carrying capacity by one third. Sell one third of stock to Bank for $500 per pen.'
+        stockSaleYield = -1 * ((sheepAmount / 200) / 3) * 500
+        // setMoney(moneyValue + stockSaleYield)
+        // setSheep((sheepAmount - ((sheepAmount / 200) / 3)))
+        stockSaleMessage = "You sold " + (sheepAmount / 3) + " sheep for " + stockSaleYield + "."
+        break;
+      }
+      case 16: {
+        message = 'Paddocks require spelling due to heavy sheep Worm infestation. Sell half of your stock at $500 per pen.'
+        stockSaleYield = -1 * ((sheepAmount / 200) / 2) * 500
+        // setMoney(moneyValue + stockSaleYield)
+        // setSheep(sheepAmount / 2)
+        stockSaleMessage = "You sold " + (sheepAmount / 2) + " sheep for " + stockSaleYield + "."
+        break;
+      }
+      case 17: {
+        message = 'You must pay $50 Income Tax for each Natural Pasture Paddock and $200 for each $1,000 Cash held, to nearest thousand.'
+        let taxOnCash = Math.floor(moneyValue / 1000) * 200
+        setMoney(moneyValue - (300 + taxOnCash))
+        break;
+      }
+      case 18: {
+        message = 'Successful lambing season due to good mangement - 120% Lambs. Receive additional 600 stock, i.e, 3 pens of sheep, or your property is already fully stocked, receive $1,200 instead.'
+        if (sheepAmount < 3000) {
+          setSheep(sheepAmount + 600)
+        }
+        if (sheepAmount >= 3000) {
+          setMoney(moneyValue + 1200)
+        }
+        break;
+      }
+      case 19: {
+        message = 'Receive $800 Compensation for State Highway put through your property.'
+        setMoney(moneyValue + 800)
+        break;
+      }
+      case 20: {
+        message = 'From sale of Fat Lambs receive $500.'
+        setMoney(moneyValue + 500)
+        break;
+      }
+      case 21: {
+        message = 'Wild Dogs kill many Sheep. If you own Stock, return 3 pens of Sheep to the Bank.'
+        setSheep(sheepAmount - 600)
+        break;
+      }
+      case 22: {
+        message = 'You have won Soil Conservation Trophy - Receive $300.'
+        setSheep(sheepAmount + 300)
+        break;
+      }
+      case 23: {
+        message = 'Good Autumn and Spring Rains. Have an additional Wool Sale. These rains break drought.'
+        woolsale = 'Yes, good luck!';
+        woolSaleYield = pens * 250
+        setMoney(moneyValue + woolSaleYield)
+        droughtMessage = 'General drought'
+        //setDrought(false)
+        break;
+      }
+      case 24: {
+        message = 'You have increased the average Fleece weight by selective breeding and good mangement, Receive bonus of $1,000.'
+        setMoney(moneyValue + 500)
         break;
       }
       default: {
@@ -132,9 +243,7 @@ const Move: React.FC = () => {
     let howMany = howManyPensForStockSale()
     let price = stockBuyPrice()
     stockSaleYield = -1 * howMany * price
-    if (moneyValue > stockSaleYield) {
-       setMoney(moneyValue + stockSaleYield)
-     }
+    setMoney(moneyValue + stockSaleYield)
     setSheep(sheepAmount + howMany * 200)
     message = 'Good luck at the stock sale.'
     stockSaleMessage = 'You sold ' + howMany + ' pens at ' + price + ' each for a total of ' + stockSaleYield + '.'
@@ -148,11 +257,9 @@ const Move: React.FC = () => {
     let howManySheep = howManyPens * 200
     let price = stockSellPrice()
     stockSaleYield = howManyPens * price
-    if (sheepAmount > howManySheep) {
-      setMoney(moneyValue + stockSaleYield)
-      setSheep(sheepAmount + howManySheep)
-      stockSaleMessage = 'You bought ' + howManyPens + ' pens at ' + price + ' each for a total of ' + stockSaleYield + '.'
-    }
+    setMoney(moneyValue + stockSaleYield)
+    setSheep(sheepAmount + howManySheep)
+    stockSaleMessage = 'You bought ' + howManyPens + ' pens at ' + price + ' each for a total of ' + stockSaleYield + '.'
   }
 
   // BORE DRIES UP
@@ -225,20 +332,19 @@ const Move: React.FC = () => {
 
   // LOCAL DROUGHT
   if (modded_position === 22 || modded_position === 43) {
+    stocksale = 'Yes'
     message = "You have a local drought and must sell half your sheep."
     let howMany = ((sheepAmount / 200) / 2)
     let price = stockSellPrice()
     stockSaleYield = -1 * howMany * price
-    if (moneyValue > stockSaleYield) {
-       setMoney(moneyValue + stockSaleYield)
-       setSheep(sheepAmount / 2)
-    }
-    stockSaleMessage = "You sold " + howMany * 200 + " sheep for " + stockSaleYield + "."
+    setMoney(moneyValue + stockSaleYield)
+    setSheep(sheepAmount / 2)
+    stockSaleMessage = "You sold " + howMany * 200 + " sheep for $" + stockSaleYield + "."
   }
 
   // LOCAL RAIN
   if (modded_position === 27) {
-    message = "Local Rain relieves you of any General Drought."
+    message = "Local Rain breaks drought."
     //setDrought(false)
     droughtMessage = "No drought."
   }
@@ -251,10 +357,10 @@ const Move: React.FC = () => {
       <div>Drought Status : {droughtMessage}</div>
       <div>Haymaking season this turn? : {haymaking}</div>
       <br></br>
-      {/* <div>Previous position was : {positionValue}</div>
-      <div>Previous event was : {board_position_array[positionValue]}</div> */}
-      {/* <div>Roll was : {dieRoll}</div>
-      <div>New position is : {modded_position}</div> */}
+      <div>Previous position was : {positionValue}</div>
+      <div>Previous event was : {board_position_array[positionValue]}</div>
+      <div>Roll was : {dieRoll}</div>
+      <div>New position is : {modded_position}</div> 
       <div>Your current event is : {board_position_array[modded_position]}</div>
       <br></br>
       <div>{message}</div>
